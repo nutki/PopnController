6950db6cc53b735df672aa2c9b05a225a783d2a3
Michal Szafranski <szafranski@gmail.com>
2026-05-14 21:38:03 +0200

    cleanup

--- ArduinoPopnController/Joystick.cpp
+++ ArduinoPopnController/Joystick.cpp
@@ -2,109 +2,54 @@
 #include "DynamicHID.h"
 
 #define JOYSTICK_REPORT_ID         0x04
+#define LED_DEFINITION(idx) 0x05, 0x0a, 0x09, (idx), 0xa1, 0x02, 0x05, 0x08, 0x09, 0x4b, 0x79, 0x03 + (idx), 0x75, 0x08, 0x95, 0x01, 0x91, 0x02, 0xc0
+const uint8_t hidReportDescriptor[] = {
+	0x05, 0x01,                 // USAGE_PAGE (Generic Desktop)
+	0x09, 0x05,                 // USAGE (Gamepad)
+	0xa1, 0x01,                 // COLLECTION (Application)
+	0x85, JOYSTICK_REPORT_ID,   // REPORT_ID
+	0x05, 0x09,                 // USAGE_PAGE (Button)
+	0x19, 0x01,                 // USAGE_MINIMUM (Button 1)
+	0x29, 0x09,                 // USAGE_MAXIMUM (Button 9)
+	0x15, 0x00,                 // LOGICAL_MINIMUM (0)
+	0x25, 0x01,                 // LOGICAL_MAXIMUM (1)
+	0x75, 0x01,                 // REPORT_SIZE (1)
+	0x95, 0x09,                 // REPORT_COUNT (# of buttons)
+	0x55, 0x00,                 // UNIT_EXPONENT (0)
+	0x65, 0x00,                 // UNIT (None)
+	0x81, 0x02,                 // INPUT (Data,Var,Abs)
+	0x75, 0x01,                 // REPORT_SIZE (1) - PADDING: 7 bits to next byte boundary
+	0x95, 0x07,                 // REPORT_COUNT (# of padding bits)
+	0x81, 0x03,                 // INPUT (Const,Var,Abs)
+	0x05, 0x01,                 // USAGE_PAGE (Generic Desktop)
+	0x09, 0x01,                 // USAGE (Pointer)
+	0x15, 0x00,                 // LOGICAL_MINIMUM (0)
+	0x26, 0xFF, 0x00,           // LOGICAL_MAXIMUM (255)
+	0x75, 0x08,                 // REPORT_SIZE (8)
+	0x95, 0x02,                 // REPORT_COUNT (2)
+	0xA1, 0x00,                 // COLLECTION (Physical)
+	0x09, 0x30,                 // USAGE (X)
+	0x09, 0x31,                 // USAGE (Y)
+	0x81, 0x02,                 // INPUT (Data,Var,Abs)
+	0xc0,                       // END_COLLECTION (Physical)        
+	// Lights - REPORT_ID 5 
+	0x85, 0x05,                 // REPORT_ID 5
+	0x15, 0x00,                 // LOGICAL_MINIMUM (0)
+	0x26, 0xFF, 0x00,           // LOGICAL_MAXIMUM (255)
+	LED_DEFINITION(1),
+	LED_DEFINITION(2),
+	LED_DEFINITION(3),
+	LED_DEFINITION(4),
+	LED_DEFINITION(5),
+	LED_DEFINITION(6),
+	LED_DEFINITION(7),
+	LED_DEFINITION(8),
+	LED_DEFINITION(9),
+	0xc0                        // END_COLLECTION
+};
 
 Joystick_::Joystick_()
 {
-    static const uint8_t hidReportDescriptor[] = {
-        // USAGE_PAGE (Generic Desktop)
-        0x05, 0x01,
-        // USAGE (Joystick - 0x04; Gamepad - 0x05; Multi-axis Controller - 0x08)
-        0x09, 0x05, // JOYSTICK_TYPE_GAMEPAD
-        // COLLECTION (Application)
-        0xa1, 0x01,
-        // REPORT_ID (Default: 3)
-        0x85, JOYSTICK_REPORT_ID,
-        
-        // USAGE_PAGE (Button)
-        0x05, 0x09,
-        // USAGE_MINIMUM (Button 1)
-        0x19, 0x01,
-        // USAGE_MAXIMUM (Button 32)
-        0x29, 0x09,
-        // LOGICAL_MINIMUM (0)
-        0x15, 0x00,
-        // LOGICAL_MAXIMUM (1)
-        0x25, 0x01,
-        // REPORT_SIZE (1)
-        0x75, 0x01,
-        // REPORT_COUNT (# of buttons)
-        0x95, 0x09,
-        // UNIT_EXPONENT (0)
-        0x55, 0x00,
-        // UNIT (None)
-        0x65, 0x00,
-        // INPUT (Data,Var,Abs)
-        0x81, 0x02,
-        
-        // Padding Bits Needed
-        // REPORT_SIZE (1)
-        0x75, 0x01,
-        // REPORT_COUNT (# of padding bits)
-        0x95, 0x07,
-        // INPUT (Const,Var,Abs)
-        0x81, 0x03,
-        
-        // USAGE_PAGE (Generic Desktop)
-        0x05, 0x01,
-        // USAGE (Pointer)
-        0x09, 0x01,
-        // LOGICAL_MINIMUM (0)
-        0x15, 0x00,
-        // LOGICAL_MAXIMUM (255)
-        0x26, 0xFF, 0x00,
-        // REPORT_SIZE (8)
-        0x75, 0x08,
-        // REPORT_COUNT (axisCount)
-        0x95, 0x02,
-        // COLLECTION (Physical)
-        0xA1, 0x00,
-        // USAGE (X)
-        0x09, 0x30,
-        // USAGE (Y)
-        0x09, 0x31,
-        // INPUT (Data,Var,Abs)
-        0x81, 0x02,
-        // END_COLLECTION (Physical)
-        0xc0,
-        
-        /*Lights */
-        /*   REPORT_ID 5*/ 
-        0x85, 0x05,
-        /*     LOGICAL_MINIMUM (0) */ 
-        0x15, 0x00,
-        /*     LOGICAL_MAXIMUM (255) */ 
-        0x26, 0xFF, 0x00,
-        /*
-        ^      HID_REPORT_ID(REPORT_ID_LIGHTS)                                      \
-        +      HID_REPORT_COUNT(60), HID_REPORT_SIZE(8),                            \
-        ^      HID_LOGICAL_MIN(0x00), HID_LOGICAL_MAX_N(0x00ff, 2),                 \
-        +      HID_USAGE_PAGE(HID_USAGE_PAGE_ORDINAL),                              \
-        ~     HID_STRING_MINIMUM(6), HID_STRING_MAXIMUM(65),                       \
-        ~     HID_USAGE_MIN(1), HID_USAGE_MAX(255),                                \
-        +      HID_OUTPUT(HID_DATA | HID_VARIABLE | HID_ABSOLUTE),                  \
-        */
-        /*Led 1 */ /* USAGE_PAGE (Ordinals), USAGE (Instance 1), COLLECTION (Logical), USAGE_PAGE (LEDs), USAGE (Generic Indicator 1), STRING INDEX (4), REPORT_SIZE (8), REPORT_COUNT (1), OUTPUT (Data,Var,Abs), END_COLLECTION */ 
-        0x05, 0x0a, 0x09, 0x01, 0xa1, 0x02, 0x05, 0x08, 0x09, 0x4b, 0x79, 0x04, 0x75, 0x08, 0x95, 0x01, 0x91, 0x02, 0xc0,
-        /*Led 2 */
-        0x05, 0x0a, 0x09, 0x02, 0xa1, 0x02, 0x05, 0x08, 0x09, 0x4b, 0x79, 0x05, 0x75, 0x08, 0x95, 0x01, 0x91, 0x02, 0xc0,
-        /*Led 3 */
-        0x05, 0x0a, 0x09, 0x03, 0xa1, 0x02, 0x05, 0x08, 0x09, 0x4b, 0x79, 0x06, 0x75, 0x08, 0x95, 0x01, 0x91, 0x02, 0xc0,
-        /*Led 4 */
-        0x05, 0x0a, 0x09, 0x04, 0xa1, 0x02, 0x05, 0x08, 0x09, 0x4b, 0x79, 0x07, 0x75, 0x08, 0x95, 0x01, 0x91, 0x02, 0xc0,
-        /*Led 5 */
-        0x05, 0x0a, 0x09, 0x05, 0xa1, 0x02, 0x05, 0x08, 0x09, 0x4b, 0x79, 0x08, 0x75, 0x08, 0x95, 0x01, 0x91, 0x02, 0xc0,
-        /*Led 6 */
-        0x05, 0x0a, 0x09, 0x06, 0xa1, 0x02, 0x05, 0x08, 0x09, 0x4b, 0x79, 0x09, 0x75, 0x08, 0x95, 0x01, 0x91, 0x02, 0xc0,
-        /*Led 7 */
-        0x05, 0x0a, 0x09, 0x07, 0xa1, 0x02, 0x05, 0x08, 0x09, 0x4b, 0x79, 0x0a, 0x75, 0x08, 0x95, 0x01, 0x91, 0x02, 0xc0,
-        /*Led 8 */
-        0x05, 0x0a, 0x09, 0x08, 0xa1, 0x02, 0x05, 0x08, 0x09, 0x4b, 0x79, 0x0b, 0x75, 0x08, 0x95, 0x01, 0x91, 0x02, 0xc0,
-        /*Led 9 */
-        0x05, 0x0a, 0x09, 0x09, 0xa1, 0x02, 0x05, 0x08, 0x09, 0x4b, 0x79, 0x0c, 0x75, 0x08, 0x95, 0x01, 0x91, 0x02, 0xc0,
-        
-        // END_COLLECTION
-        0xc0
-    };
 
 	// Register HID Report Description
 	DynamicHIDSubDescriptor *node = new DynamicHIDSubDescriptor(hidReportDescriptor, sizeof(hidReportDescriptor), true);
